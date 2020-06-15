ba0c0234293737330c6beab803b64009f654bbd7
Jing Liu <shliujing@foxmail.com>
2020-06-15 17:59:26 +0800

    add.

--- test/test_db_batch.py
+++ test/test_db_batch.py
@@ -11,18 +11,13 @@ c = conn.cursor()
 # 往表里面插入数据
 # c.execute("INSERT INTO article ('content') VALUES ('22asdad打算打算'),  ('11asdad打算打算') ")
 sql_values = 'INSERT INTO article VALUES '
-datas= [("aavas"),("aas87111")]
-for i in range(0, len(datas)):  # 列表下标索引，一一提取一行数据
-    sql_values += '('  # 增加execute语句所需的左括号
-    sql_values += datas[i] # 插入数据
-    sql_values += '),'  # 右括号
-sql_values = sql_values.strip(',')  # 去除最后一行数据的逗号，也可replace为分号
-sql_values = """INSERT INTO article (content) VALUES (aavas),(aas87111)"""
+datas="111aavas"
+# sql = "Insert Into p ({1}) Values('张三', '语文', '90')".format(table_name, fields_name)
+# sql_values = "Insert Into p Values(\"\"\"{1}\"\"\")".format(datas)
+sql_values = "Insert Into p Values({0})".format(datas)
 print(sql_values)
 c.execute(sql_values)
 
-cursor = c.execute("SELECT content from article")
-for row in cursor:
-    print("content = ", row[0])
+cursor = c.execute(sql_values)
 conn.commit()
 conn.close()
